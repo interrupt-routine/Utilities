e41192583e57d74e826e4836dc2a65995ad01655
interrupt-routine <77172657+interrupt-routine@users.noreply.github.com>
2022-04-16 12:14:15 +0200

    Create asprintf.c

---
+++ asprintf.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int vasprintf (char **string_ptr, const char *format, va_list args)
+{
+	va_list copy;
+	va_copy(copy, args);
+
+	int nb_chars = vsnprintf(NULL, 0, format, copy);
+	va_end(copy);
+
+	if (nb_chars < 0)
+		return nb_chars;
+
+// we use another pointer in case *string_ptr is used also used as a format argument
+// otherwise, there would be overlap
+	char *string = malloc(nb_chars + 1);
+
+	if (string == NULL)
+		return -1;
+
+	int retval = vsprintf(string, format, args);
+
+	if (retval < 0) {
+		free(string);
+		return retval;
+	}
+
+	*string_ptr = string;
+	return retval;
+}
+
+int asprintf (char **string_ptr, const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	int retval = vasprintf(string_ptr, format, args);
+	va_end(args);
+
+	return retval;
+}
